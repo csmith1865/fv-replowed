83f00838c4411aa5c6df9c49e98abf836fe4f16c
Kehayeah <b.keh97@hotmail.com>
2025-02-10 22:42:12 +0200

    Update config.php

--- public/farmville/flashservices/amfphp/Helpers/config.php
+++ public/farmville/flashservices/amfphp/Helpers/config.php
@@ -2,6 +2,6 @@
 
 // database credentials
 define('DB_SERVER', 'localhost'); // replace with your host name or IP address
-define('DB_USERNAME', 'root'); // replace with your database username
+define('DB_USERNAME', ''); // replace with your database username
 define('DB_PASSWORD', ''); // replace with your database password
-define('DB_NAME', 'laratests'); // replace with your database name
+define('DB_NAME', ''); // replace with your database name
