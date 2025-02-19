044be066b8bf1ebbfee2649942fef45e3d5b7e7a
Kehayeah <b.keh97@hotmail.com>
2025-02-19 22:11:28 +0200

    Fixed intentional typo in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @if (is_dir(public_path('farmville/assets/hashed/assetss')))
+                    @if (is_dir(public_path('farmville/assets/hashed/assets')))
                         <h2>Assets exist, Go to the Play tab and enjoy</h2>
                     @else
                         <h2>Assets don't exist.</h2>
